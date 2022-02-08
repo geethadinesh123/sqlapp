9116bd96602c04e0272126d39071edfa8b7a7c81
geetha.naidu <geetha.naidu@M071019-JMR>
2022-02-08 20:29:44 +0100

    Ajoutez des fichiers projet.

---
+++ Controllers/CourseController.cs
@@ -0,0 +1,98 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using sqlapp.Models;
+using sqlapp.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace sqlapp.Controllers
+{
+    public class CourseController : Controller
+    {
+        private readonly CourseService _course_service;
+        private readonly IConfiguration _configuration;
+        public CourseController(CourseService _svc, IConfiguration _config)
+        {
+            _course_service = _svc;
+            _configuration = _config;
+        }
+        // GET: Course
+        public IActionResult Index()
+        {
+            IEnumerable<Course> _course_list = _course_service.GetCourses(_configuration.GetConnectionString("SQLConnection"));
+            return View(_course_list);
+        }
+
+        // GET: Course/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: Course/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Course/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Course/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: Course/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Course/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: Course/Delete/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Delete(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
---
+++ Services/CourseService.cs
@@ -0,0 +1,41 @@
+﻿using sqlapp.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace sqlapp.Services
+{
+    public class CourseService
+    {
+        private string conStr = "Server=tcp:imgsqlservergeetha.database.windows.net,1433;Initial Catalog=imgsqldbgeetha;Persist Security Info=False;User ID=geetha;Password=Sairam@123;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;";
+        private SqlConnection GetConnection()
+        {
+            return new SqlConnection(conStr);
+        }
+
+        public IEnumerable<Course> GetCourses(string conStr)
+        {
+            List<Course> _list = new List<Course>();
+            string _statement = "select * from Course";
+            SqlConnection _conn = new SqlConnection(conStr);
+            _conn.Open();
+            SqlCommand _cmd = new SqlCommand(_statement, _conn);
+            using (SqlDataReader _reader=_cmd.ExecuteReader())
+            {
+                while (_reader.Read())
+                {
+                    Course _course = new Course()
+                    {
+                        CourseID=_reader.GetInt32(0),
+                        CourseName=_reader.GetString(1),
+                        Rating=_reader.GetDecimal(2)
+                    };
+                    _list.Add(_course);
+                }
+            }
+            return _list;
+        }
+    }
+}
